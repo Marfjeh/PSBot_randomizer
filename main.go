8b83682a31e72b86f6f8040c1b34765da1059563
Ted van Riel <80752652+tvanriel@users.noreply.github.com>
2025-12-19 10:09:51 +0100

    feat: log in logfmt and format

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
+	"log/slog"
 	"math/rand/v2"
 	"net/http"
 	"os"
@@ -22,7 +22,7 @@ type Config struct {
 }
 
 type Event struct {
-	Name			string	 `json:"name"`
+	Name      string   `json:"name"`
 	RandomMin string   `json:"random_min"`
 	RandomMax string   `json:"random_max"`
 	Sounds    []string `json:"sounds"`
@@ -34,23 +34,28 @@ type PsbotBody struct {
 	Sound string `json:"sound"`
 }
 
-//Calcuate a random time between two values and return that.
+// Calculate a random time between two values and return that.
 func randomDuration(min, max time.Duration) time.Duration {
 	r := rand.New(rand.NewPCG(1, uint64(time.Now().UnixNano())))
 	return min + time.Duration(r.Int64N(int64(max-min)))
 }
 
-//Send an POST request to the server to play the sound
-func playSound(ctx context.Context, url string, UserAgent string, Psbot PsbotBody) error {
+var errNoOKResponse = errors.New("did not got a sensible response from PSBOT")
+
+// Send an POST request to the server to play the sound
+func playSound(ctx context.Context, logger *slog.Logger, url string, UserAgent string, Psbot PsbotBody) error {
 	b := bytes.NewBuffer([]byte{})
 	_ = json.NewEncoder(b).Encode(Psbot)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, b)
+	if err != nil {
+		return fmt.Errorf("create new request: %w", err)
+	}
+
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("User-Agent", UserAgent)
 
 	resp, err := http.DefaultClient.Do(req)
-
 	if err != nil {
 		return err
 	}
@@ -58,17 +63,17 @@ func playSound(ctx context.Context, url string, UserAgent string, Psbot PsbotBod
 	defer resp.Body.Close()
 
 	if resp.StatusCode >= http.StatusOK && resp.StatusCode < http.StatusBadRequest {
-		fmt.Printf("Played sound %s in guild: %s\n", Psbot.Sound, Psbot.Guild)
+		logger.Info("Played sound", "sound", Psbot.Sound, "guild", Psbot.Guild)
 	} else {
-		return fmt.Errorf("%w: %q", errors.New("Did not got a sensible response from PSBOT :("), resp.Status)
+		return fmt.Errorf("%w: %q", errNoOKResponse, resp.Status)
 	}
 
 	return nil
 }
 
-//Setup thread
-func StartPlaying(ctx context.Context, e Event, guildID string, psbotURL string) error {
-	//Seed the randomizer
+// Setup thread
+func StartPlaying(ctx context.Context, logger *slog.Logger, e Event, guildID string, psbotURL string) error {
+	// Seed the randomizer
 	r := rand.New(rand.NewPCG(1, uint64(time.Now().UnixNano())))
 	randomMin, err := time.ParseDuration(e.RandomMin)
 	if err != nil {
@@ -83,15 +88,15 @@ func StartPlaying(ctx context.Context, e Event, guildID string, psbotURL string)
 X:
 	for {
 		randomTime := randomDuration(randomMin, randomMax)
-		log.Printf("[%s] Waiting for: %q\n", e.Name, randomTime.String())
+		logger.Info("Sleeping", "duration", randomTime.String())
 		select {
 		case <-ctx.Done():
 			break X
 		case <-time.Tick(randomTime):
 			randomSoundIndex := r.IntN(len(e.Sounds))
-			err := playSound(ctx, psbotURL, e.UserAgent, PsbotBody{Guild: guildID, Sound: e.Sounds[randomSoundIndex]})
+			err := playSound(ctx, logger, psbotURL, e.UserAgent, PsbotBody{Guild: guildID, Sound: e.Sounds[randomSoundIndex]})
 			if err != nil {
-				log.Printf("Something went fucky wucky when playing a sound %v", err)
+				logger.Error("Playing sound", "err", err, "sound", e.Sounds[randomSoundIndex])
 			}
 		}
 	}
@@ -104,22 +109,26 @@ func main() {
 
 	eg, ctx2 := errgroup.WithContext(ctx)
 
+	logger := slog.Default()
+
 	cfg, err := os.Open("./config/config.json")
 	if err != nil {
-		log.Fatalf("Unable to read config file! %v", err)
+		logger.Error("Read config file!", "err", err)
+		return
 	}
 
 	c := Config{}
-	err = json.NewDecoder(cfg).Decode(&c)
+	_ = json.NewDecoder(cfg).Decode(&c)
 
 	for _, e := range c.Events {
 		eg.Go(func() error {
-			return StartPlaying(ctx2, e, c.GuildID, c.PsbotURL)
+			return StartPlaying(ctx2, logger.With("name", e.Name), e, c.GuildID, c.PsbotURL)
 		})
 	}
 
 	err = eg.Wait()
 	if err != nil {
-		log.Fatalf("One of the threads got killed %v", err)
+		logger.Error("One of the threads got killed", "err", err)
+		return
 	}
 }
